051063871084826da6c1571e19b5d9d71def46a1
Moinuddin <moinuddinchudiwal@gmail.com>
2024-12-02 21:51:33 +0530

    content adde in quality and Assurance page

--- src/pages/qualityAssurance/index.jsx
+++ src/pages/qualityAssurance/index.jsx
@@ -7,6 +7,14 @@ import Paragraph from "../../components/Paragraph";
 const index = () => {
   const { qualityAssurance } = data.binsinaPharma;
 
+  const checklistColors = [
+    "bg-teal-500",
+    "bg-pink-500",
+    "bg-yellow-500",
+    "bg-blue-500",
+    "bg-indigo-500",
+  ];
+
   return (
     <>
       <Banner
@@ -15,165 +23,64 @@ const index = () => {
         backgroundImage={qualityAssurance.bannerImage}
       />
 
-      <section className='md:px-16 px-6 md:pt-28 pt-12'>
-        <div className='flex flex-col md:flex-row items-start gap-16'>
-          {/* Left Image Section */}
-          <div className='w-full md:w-1/2 relative flex flex-col space-y-4'>
-            <img
-              src={
-                "https://cdn.pixabay.com/photo/2023/12/11/11/32/ai-generated-8443395_1280.jpg"
-              }
-              alt='Truck'
-              className='w-[600px] max-lg:w-full h-[500px] max-lg:h-auto object-cover rounded-md shadow-lg'
-            />
-          </div>
+      {qualityAssurance.content.map((item, index) => {
+        const isImageLeft = index % 2 === 0;
+        const isLastSection = index === qualityAssurance.content.length - 1;
 
-          {/* Right Info Section */}
-          <div className='w-full md:w-1/2'>
-            <div className='space-y-6'>
-              <BorderTitle title='Our Values' />
-              <Heading title='Ruchi Cargo Agency' align='start' />
-              <Paragraph
-                title='Ruchi Cargo Agency was established and promoted by a group of
-              shipping professionals, and are in the prime business of Shipping
-              Agency, Customs Broking, Freight Forwarding, Stevedoring, Vessel
-              Agency, and Chartering Agency Project cargo operator, Liasoning
-              Agent.'
-              />
-            </div>
-            <div className='mt-8 flex gap-10'>
-              <ul className='space-y-2'>
-                <li className='flex items-center'>
-                  <span className='text-text-color text-base mr-2 bg-teal-500 w-6 h-6 flex items-center justify-center rounded-full text-white'>
-                    ✔
-                  </span>
-                  <span className='text-gray-800 font-semibold'>
-                    CUSTOM CLEARING
-                  </span>
-                </li>
-                <li className='flex items-center'>
-                  <span className='text-text-color text-base mr-2 bg-pink-500 w-6 h-6 flex items-center justify-center rounded-full text-white'>
-                    ✔
-                  </span>
-                  <span className='text-gray-800 font-semibold'>
-                    SEA FREIGHT
-                  </span>
-                </li>
-                <li className='flex items-center'>
-                  <span className='text-text-color text-base mr-2 bg-yellow-500 w-6 h-6 flex items-center justify-center rounded-full text-white'>
-                    ✔
-                  </span>
-                  <span className='text-gray-800 font-semibold'>
-                    AIR FREIGHT
-                  </span>
-                </li>
-                <li className='flex items-center'>
-                  <span className='text-text-color text-base mr-2 bg-blue-500 w-6 h-6 flex items-center justify-center rounded-full text-white'>
-                    ✔
-                  </span>
-                  <span className='text-gray-800 font-semibold'>
-                    AIR FREIGHT
-                  </span>
-                </li>
-                <li className='flex items-center'>
-                  <span className='text-text-color text-base mr-2 bg-indigo-500 w-6 h-6 flex items-center justify-center rounded-full text-white'>
-                    ✔
-                  </span>
-                  <span className='text-gray-800 font-semibold'>
-                    AIR FREIGHT
-                  </span>
-                </li>
-              </ul>
-            </div>
-            <Paragraph
-              title='Ruchi Cargo Agency was established and promoted by a group of
-              shipping professionals, and are in the prime business of Shipping
-              Agency.'
-              className='mt-4'
-            />
-          </div>
-        </div>
-      </section>
+        return (
+          <section
+            className={`md:px-16 px-6 ${
+              isLastSection ? "md:pt-28 pt-12 mb-28" : "md:pt-28 pt-12"
+            }`}
+            key={index}
+          >
+            <div
+              className={`flex flex-col ${
+                isImageLeft ? "md:flex-row" : "md:flex-row-reverse"
+              } items-start gap-16`}
+            >
+              <div className='w-full md:w-1/2 relative flex flex-col space-y-4'>
+                <img
+                  src={item.image}
+                  alt={`Section ${index}`}
+                  className='w-[600px] max-lg:w-full h-[500px] max-lg:h-auto object-cover rounded-md shadow-lg'
+                />
+              </div>
 
-      <section className='md:px-16 px-6 md:py-28 py-12'>
-        <div className='flex flex-col md:flex-row-reverse items-start gap-16'>
-          {/* Left Image Section */}
-          <div className='w-full md:w-1/2 relative flex flex-col space-y-4'>
-            <img
-              src={
-                "https://cdn.pixabay.com/photo/2023/12/11/11/32/ai-generated-8443395_1280.jpg"
-              }
-              alt='Truck'
-              className='w-[600px] max-lg:w-full h-[500px] max-lg:h-auto object-cover rounded-md shadow-lg'
-            />
-          </div>
+              <div className='w-full md:w-1/2'>
+                <div className='space-y-6'>
+                  <BorderTitle title={item.title} />
+                  <Heading title={item.heading} align='start' />
+                  <Paragraph title={item.paragraph} />
+                </div>
 
-          {/* Right Info Section */}
-          <div className='w-full md:w-1/2'>
-            <div className='space-y-6'>
-              <BorderTitle title='Our Values' />
-              <Heading title='Ruchi Cargo Agency' align='start' />
-              <Paragraph
-                title='Ruchi Cargo Agency was established and promoted by a group of
-              shipping professionals, and are in the prime business of Shipping
-              Agency, Customs Broking, Freight Forwarding, Stevedoring, Vessel
-              Agency, and Chartering Agency Project cargo operator, Liasoning
-              Agent.'
-              />
+                <div className='mt-8'>
+                  <ul className='space-y-2'>
+                    {item.checklist?.map((checkItem, checkIndex) => {
+                      const colorClass =
+                        checklistColors[checkIndex % checklistColors.length];
+                      return (
+                        <li className='flex items-center' key={checkIndex}>
+                          <span
+                            className={`text-text-color text-base mr-2 ${colorClass} w-6 h-6 flex items-center justify-center rounded-full text-white`}
+                          >
+                            ✔
+                          </span>
+                          <span className='text-gray-800 font-semibold'>
+                            {checkItem}
+                          </span>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+
+                <Paragraph title={item.additionalParagraph} className='mt-4' />
+              </div>
             </div>
-            <div className='mt-8 flex gap-10'>
-              <ul className='space-y-2'>
-                <li className='flex items-center'>
-                  <span className='text-text-color text-base mr-2 bg-teal-500 w-6 h-6 flex items-center justify-center rounded-full text-white'>
-                    ✔
-                  </span>
-                  <span className='text-gray-800 font-semibold'>
-                    CUSTOM CLEARING
-                  </span>
-                </li>
-                <li className='flex items-center'>
-                  <span className='text-text-color text-base mr-2 bg-pink-500 w-6 h-6 flex items-center justify-center rounded-full text-white'>
-                    ✔
-                  </span>
-                  <span className='text-gray-800 font-semibold'>
-                    SEA FREIGHT
-                  </span>
-                </li>
-                <li className='flex items-center'>
-                  <span className='text-text-color text-base mr-2 bg-yellow-500 w-6 h-6 flex items-center justify-center rounded-full text-white'>
-                    ✔
-                  </span>
-                  <span className='text-gray-800 font-semibold'>
-                    AIR FREIGHT
-                  </span>
-                </li>
-                <li className='flex items-center'>
-                  <span className='text-text-color text-base mr-2 bg-blue-500 w-6 h-6 flex items-center justify-center rounded-full text-white'>
-                    ✔
-                  </span>
-                  <span className='text-gray-800 font-semibold'>
-                    AIR FREIGHT
-                  </span>
-                </li>
-                <li className='flex items-center'>
-                  <span className='text-text-color text-base mr-2 bg-indigo-500 w-6 h-6 flex items-center justify-center rounded-full text-white'>
-                    ✔
-                  </span>
-                  <span className='text-gray-800 font-semibold'>
-                    AIR FREIGHT
-                  </span>
-                </li>
-              </ul>
-            </div>
-            <Paragraph
-              title='Ruchi Cargo Agency was established and promoted by a group of
-              shipping professionals, and are in the prime business of Shipping
-              Agency.'
-              className='mt-4'
-            />
-          </div>
-        </div>
-      </section>
+          </section>
+        );
+      })}
     </>
   );
 };

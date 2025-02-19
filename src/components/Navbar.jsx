5396fce11be1e4cedc6f7292fb3ccff887310b91
kasimkkn <kasimkkn15@gmail.com>
2025-02-19 10:06:04 +0530

    updated logo by code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,8 @@ import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import { FaInstagram } from "react-icons/fa";
 import { RiArrowDownSLine, RiTwitterXLine } from "react-icons/ri";
 import { Link, useLocation } from "react-router-dom";
-import logo from "../assets/img/logo.png";
+import logo from "../assets/img/favicon.png";
+import logoArrow from "../assets/img/logo-arrow.png";
 import Contact from "../models/Contact";
 import Button from "./Button/Button";
 import data from "../data/common.json";
@@ -55,7 +56,6 @@ const Navbar = ({ menu, setMenu, showForm, openForm, closeForm }) => {
 
   return (
     <nav className='fixed w-full z-10 text-white'>
-      {/* top Bar */}
       <aside
         className={`transition-transform duration-300 ${hideTopBar ? "-translate-y-full" : "translate-y-0"
           } flex md:flex-row flex-col max-md:items-end md:justify-between p-4 border-b border-gray-200 lg:px-16 px-8 bg-gradient-to-r from-brightColor to-[#1a1b26]`}
@@ -146,21 +146,27 @@ const Navbar = ({ menu, setMenu, showForm, openForm, closeForm }) => {
         </div>
       </aside>
 
-      {/* Navbar */}
       <header
-        className={`transition-transform duration-300 ${hideTopBar ? "-translate-y-[75%]" : "translate-y-0"
+        className={`transition-transform duration-300 ${hideTopBar ? "-translate-y-[85%]" : "translate-y-0"
           }`}
       >
         <div className='relative flex flex-row justify-between items-center p-4 md:px-16 bg-bgHead shadow-md'>
           <Link
             to='/'
-            className='flex flex-row lg:w-[400px] w-72 items-center cursor-pointer'
+            className='flex items-center cursor-pointer max-md:gap-2'
           >
             <img
               src={logo}
               alt='Logo'
-              className='object-contain w-full h-full'
+              className='object-contain w-12 h-full'
             />
+            <div className="flex flex-col items-end gap-[3px]">
+              <span className="text-brightColor font-bold sm:text-[13px] text-xs">Binsina Pharma Export Pvt Ltd <sup className="text-[9px]">TM</sup> </span>
+              <div className="flex items-center">
+                <img src={logoArrow} alt="logo arrow" className="sm:w-[40%] w-[10px]" />
+                <span className="text-backgroundColor font-bold text-[10px]">Exporting Quality, Sincerely...</span>
+              </div>
+            </div>
           </Link>
 
           {/* Desktop Navigation */}
@@ -178,7 +184,7 @@ const Navbar = ({ menu, setMenu, showForm, openForm, closeForm }) => {
                     className={`text-black text-base transition-all duration-300 ease-in-out cursor-pointer group-hover:text-hoverColor ${location.pathname === link.path ? "text-hoverColor" : ""
                       }`}
                   >
-                    <span className="relative after:content-[''] after:absolute after:bottom-[-4px] after:left-1/2 after:h-[2px] after:w-0 after:bg-hoverColor after:transition-all after:duration-300 after:ease-in-out after:transform after:origin-center group-hover:after:w-full group-hover:after:left-0">
+                    <span className="text-sm relative after:content-[''] after:absolute after:bottom-[-4px] after:left-1/2 after:h-[2px] after:w-0 after:bg-hoverColor after:transition-all after:duration-300 after:ease-in-out after:transform after:origin-center group-hover:after:w-full group-hover:after:left-0">
                       {link.label}
                     </span>
                   </Link>
@@ -196,7 +202,7 @@ const Navbar = ({ menu, setMenu, showForm, openForm, closeForm }) => {
           {showForm && <Contact closeForm={closeForm} />}
 
           {/* Mobile Menu Button */}
-          <div className='lg:hidden flex items-center absolute right-5 top-8 z-50'>
+          <div className='lg:hidden flex items-center absolute right-5 top-6 z-50'>
             {menu ? (
               <AiOutlineClose
                 className='text-brightColor transition duration-700 ease-in-out'
@@ -216,7 +222,7 @@ const Navbar = ({ menu, setMenu, showForm, openForm, closeForm }) => {
         {/* Mobile Navigation */}
         <div
           className={`${menu ? "translate-x-0" : "-translate-x-full"
-            } lg:hidden flex flex-col absolute bg-gradient-to-r from-brightColor to-[#1a1b26] text-gray-700 left-0 top-[5rem] font-semibold text-2xl text-center pt-8 pb-4 gap-8 w-full h-[calc(100vh-4rem)] transition-transform duration-300`}
+            } lg:hidden flex flex-col absolute bg-gradient-to-r from-brightColor to-[#1a1b26] text-gray-700 left-0 top-[4.6rem] font-semibold text-2xl text-center pt-8 pb-4 gap-8 w-full h-[calc(100vh-4rem)] transition-transform duration-300`}
         >
           {navLinks.map((link, index) => (
             <div key={index}>
